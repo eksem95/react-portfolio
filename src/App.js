99ec1c0fcee7b98cac4e2671a7af57ff187e66fb
Megan Ekse <meganekse@gmail.com>
2021-10-26 00:03:04 -0700

    nav links, styling

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import samples from './samples';
 function App() {
   // console.log( samples )
   return (
-    <div>
+    <div className="App">
       <Header />
       <div id="about">
         <p>Hello! Welcome to my portfolio! I am a full stack web developer excited to showcase my skills!</p>
